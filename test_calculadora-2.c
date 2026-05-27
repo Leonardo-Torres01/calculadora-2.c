67b423f35cc0c8ee2a1a0c7265b482e45e2aa08a
Leonardo <leomatorres1901@gmail.com>
2026-05-26 22:07:46 -0300

    Create test_calculadora-2.c

---
+++ test_calculadora-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+#include "function_calcular.c"
+
+int main()
+{
+    assert(dividir(14,2) == 7);
+    assert(dividir(99,3) == 33);
+    assert(dividir(144,12) == 12);
+    printf("Teste executado com sucesso");
+ printf("\nfeito por Leonardo Mattoso (https://github.com/Leonardo-Torres01)");
+ return 0;
+}
